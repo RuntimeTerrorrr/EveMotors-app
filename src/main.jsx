b1a6d52f912f7323dfc9250b47da4646554ed809
Mubeen Khan <mubeen.rehman120@gmail.com>
2023-09-03 12:18:57 +0500

    Routes Changed Again

--- src/main.jsx
+++ src/main.jsx
@@ -13,11 +13,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <BrowserRouter>
         <Routes>
-          <Route path="https://evemotors-app.vercel.app//login" element={<Login />} />
-          <Route path="https://evemotors-app.vercel.app//register" element={<SimpleRegistrationForm />} />
-          <Route path="https://evemotors-app.vercel.app//" element={<Home />} />
-          <Route path="https://evemotors-app.vercel.app//dashboard" element={<FormDashboard />} />
-          <Route path="https://evemotors-app.vercel.app//addacar" element={<AddACar />} />
+          <Route path="evemotors-app.vercel.app/login" element={<Login />} />
+          <Route path="evemotors-app.vercel.app/register" element={<SimpleRegistrationForm />} />
+          <Route path="evemotors-app.vercel.app/" element={<Home />} />
+          <Route path="evemotors-app.vercel.app/dashboard" element={<FormDashboard />} />
+          <Route path="evemotors-app.vercel.app/addacar" element={<AddACar />} />
         </Routes>
       </BrowserRouter>
   </React.StrictMode>,
