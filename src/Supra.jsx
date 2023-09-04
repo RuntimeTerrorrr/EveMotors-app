c1a6b9573737f31bc91e2ea0d1659959342bd7c8
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2023-09-04 21:01:38 +0500

    Supra's Tagline changed

--- src/Supra.jsx
+++ src/Supra.jsx
@@ -12,7 +12,7 @@ const Supra = () => {
                 <div className=" rounded-[32px] mt-8 mx-8  h-[600px] bg-prime">
                 <div className='  px-8 translate-y-24'>
                 <h1 className=" font-prime text-9xl max-[1200px]:text-8xl max-[750px]:text-7xl uppercase text-white ">Toyota Supra Mk-4</h1>
-                <p className=' text-white font-custom text-3xl w-[600px]'>He become one of revolutionary cars of his time.beauty, eligance, and power.This car must you want.</p>
+                <p className=' text-white font-custom text-3xl w-[600px]'>The iconic sports car known for its timeless design and thrilling turbocharged performance.</p>
                 </div>
                 </div>
             </div>
