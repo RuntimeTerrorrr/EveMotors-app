d02cf02d09eeb30be3d30265d3f865791ca26bbd
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2023-09-06 01:15:38 +0500

    loeader Centered

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const LoaderAnimation = () => {
   return (
     <>
-    <div className='w-full h-screen flex items-center content-center'>
-    <div className="m-auto ease-linear rounded-full border-4 border-prime h-12 w-12 mb-4 border-t-8 animate-spin"></div>
+    <div className='w-full h-screen flex items-center justify-center'>
+    <div className="ease-linear rounded-full border-4 border-prime h-12 w-12 border-t-8 animate-spin"></div>
 
     </div>
     </>
