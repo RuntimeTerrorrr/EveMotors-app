2053a7ad5659412e3836b49696325847904bb9de
XhOwais <xhowais@gmail.com>
2023-09-05 10:47:04 +0500

    Register Form Image changed

--- src/Register.jsx
+++ src/Register.jsx
@@ -40,8 +40,8 @@ export default function Register() {
           >
             <img
               alt="Night"
-              src="https://images.unsplash.com/photo-1657987273009-8fe82419acc6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
-              className="absolute inset-0 h-full w-full object-cover opacity-80"
+              src="https://images.unsplash.com/photo-1657987273071-fbe77b5b4e90?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
+              className="absolute inset-0 h-full w-full object-cover opacity-80 scale-x-[-1]"
             />
 
             <div className=" absolute">
