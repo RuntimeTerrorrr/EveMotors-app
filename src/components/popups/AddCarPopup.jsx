ec28f6916876188734b1bc7d82ce943a15bd5ce4
Xh Owais <87980770+XhOwais@users.noreply.github.com>
2023-09-06 10:37:48 +0500

    AddCar Popup Added

--- src/components/popups/AddCarPopup.jsx
+++ src/components/popups/AddCarPopup.jsx
@@ -8,7 +8,7 @@ import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import 'tailwindcss/tailwind.css'; // Import Tailwind CSS
 
-const steps = ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+const steps = ['Basic Info', 'Specification', 'Images', 'Model and Cover Image'];
 
 export default function AddCarPopup() {
   const [activeStep, setActiveStep] = React.useState(0);
@@ -16,7 +16,9 @@ export default function AddCarPopup() {
   const [step1Data, setStep1Data] = React.useState('');
   const [step2Data, setStep2Data] = React.useState('');
   const [step3Data, setStep3Data] = React.useState('');
-  
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
+
 
   const totalSteps = () => {
     return steps.length;
@@ -63,7 +65,7 @@ export default function AddCarPopup() {
   };
 
   return (
-    <Box sx={{ width: '100%' }}>
+    <Box className=" flex flex-col justify-center" sx={{ width: '100%', px: '20px' , height: '400px' }}>
       <Stepper nonLinear activeStep={activeStep}>
         {steps.map((label, index) => (
           <Step key={label} completed={completed[index]}>
@@ -76,111 +78,346 @@ export default function AddCarPopup() {
       <div>
         {activeStep === 0 && (
           <>
-          <div className=' grid grid-cols-2 px-10 gap-4 mt-10'>
-          <div>
-          <label
-                    htmlFor="FirstName"
-                    className="block text-sm font-medium text-white"
-                  >
-                    Make Model
-          </label>
-          <input
-              type="text"
-              id="FirstName"
-              value={step1Data}
-              onChange={(e) => setStep1Data(e.target.value)}
-              className="mt-1 w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
-              placeholder="i.e, Mike"
-              required
-            />
-          </div>
-          <div>
-          <label
-                    htmlFor="FirstName"
-                    className="block text-sm font-medium text-white"
-                  >
+            <div className=' grid grid-cols-2 px-10 gap-4 mt-10'>
+              <div>
+                <label
+                  htmlFor="FirstName"
+                  className="block text-sm font-medium text-white"
+                >
+                  Make Model
+                </label>
+                <input
+                  type="text"
+                  id="FirstName"
+                  value={step1Data}
+                  onChange={(e) => setStep1Data(e.target.value)}
+                  className="mt-1 w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
+                  placeholder="i.e, Mike"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="FirstName"
+                  className="block text-sm font-medium text-white"
+                >
                   Varient
-          </label>
-          <input
-              type="text"
-              id="FirstName"
-              value={step1Data}
-              onChange={(e) => setStep1Data(e.target.value)}
-              className="mt-1 w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
-              placeholder="i.e, Mike"
-              required
-            />
-          </div>
-          <div>
-          <label
-                    htmlFor="FirstName"
-                    className="block text-sm font-medium text-white"
-                  >
+                </label>
+                <input
+                  type="text"
+                  id="FirstName"
+                  value={step1Data}
+                  onChange={(e) => setStep1Data(e.target.value)}
+                  className="mt-1 w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
+                  placeholder="i.e, Mike"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="FirstName"
+                  className="block text-sm font-medium text-white"
+                >
                   Registered in
-          </label>
-          <input
-              type="text"
-              id="FirstName"
-              value={step1Data}
-              onChange={(e) => setStep1Data(e.target.value)}
-              className="mt-1 w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
-              placeholder="i.e, Mike"
-              required
-            />
-          </div>
-          <div>
-          <label
-                    htmlFor="FirstName"
-                    className="block text-sm font-medium text-white"
-                  >
+                </label>
+                <input
+                  type="text"
+                  id="FirstName"
+                  value={step1Data}
+                  onChange={(e) => setStep1Data(e.target.value)}
+                  className="mt-1 w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
+                  placeholder="i.e, Mike"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="FirstName"
+                  className="block text-sm font-medium text-white"
+                >
                   Assembled in
-          </label>
-          <input
-              type="text"
-              id="FirstName"
-              value={step1Data}
-              onChange={(e) => setStep1Data(e.target.value)}
-              className="mt-1 w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
-              placeholder="i.e, Mike"
-              required
-            />
-          </div>
-          <div className=' col-start-1 col-end-3'>
-          <label
-                    htmlFor="FirstName"
-                    className="block text-sm font-medium text-white"
-                  >
-                  Assembled in
-          </label>
-          <textarea
-              id="FirstName"
-              value={step1Data}
-              onChange={(e) => setStep1Data(e.target.value)}
-              className="mt-1 h-24  w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
-              placeholder="i.e, Mike"
-              required
-            />
-          </div>
-          </div>
-            </>
+                </label>
+                <input
+                  type="text"
+                  id="FirstName"
+                  value={step1Data}
+                  onChange={(e) => setStep1Data(e.target.value)}
+                  className="mt-1 w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
+                  placeholder="i.e, Mike"
+                  required
+                />
+              </div>
+              <div className=' col-start-1 col-end-3'>
+                <label
+                  htmlFor="FirstName"
+                  className="block text-sm font-medium text-white"
+                >
+                  Intro
+                </label>
+                <textarea
+                  id="FirstName"
+                  value={step1Data}
+                  onChange={(e) => setStep1Data(e.target.value)}
+                  className="mt-1 h-24  w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
+                  placeholder="i.e, Mike"
+                  required
+                />
+              </div>
+            </div>
+          </>
         )}
         {activeStep === 1 && (
-          <TextField
-            label="Ad Group"
-            variant="outlined"
-            value={step2Data}
-            onChange={(e) => setStep2Data(e.target.value)}
-            className="w-full p-2 border rounded-md" // Apply Tailwind CSS classes here
-          />
+          <>
+            <div className=' grid grid-cols-2 px-10 gap-4 mt-10'>
+              <div>
+                <label
+                  htmlFor="FirstName"
+                  className="block text-sm font-medium text-white"
+                >
+                  Engine
+                </label>
+                <input
+                  type="text"
+                  id="FirstName"
+                  value={step1Data}
+                  onChange={(e) => setStep1Data(e.target.value)}
+                  className="mt-1 w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
+                  placeholder="i.e, Mike"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="FirstName"
+                  className="block text-sm font-medium text-white"
+                >
+                  Torque
+                </label>
+                <input
+                  type="text"
+                  id="FirstName"
+                  value={step1Data}
+                  onChange={(e) => setStep1Data(e.target.value)}
+                  className="mt-1 w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
+                  placeholder="i.e, Mike"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="FirstName"
+                  className="block text-sm font-medium text-white"
+                >
+                  Power
+                </label>
+                <input
+                  type="text"
+                  id="FirstName"
+                  value={step1Data}
+                  onChange={(e) => setStep1Data(e.target.value)}
+                  className="mt-1 w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
+                  placeholder="i.e, Mike"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="FirstName"
+                  className="block text-sm font-medium text-white"
+                >
+                  Top Speed
+                </label>
+                <input
+                  type="text"
+                  id="FirstName"
+                  value={step1Data}
+                  onChange={(e) => setStep1Data(e.target.value)}
+                  className="mt-1 w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
+                  placeholder="i.e, Mike"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="FirstName"
+                  className="block text-sm font-medium text-white"
+                >
+                  Body Type
+                </label>
+                <input
+                  type="text"
+                  id="FirstName"
+                  value={step1Data}
+                  onChange={(e) => setStep1Data(e.target.value)}
+                  className="mt-1 w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
+                  placeholder="i.e, Mike"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="FirstName"
+                  className="block text-sm font-medium text-white"
+                >
+                  Category
+                </label>
+                <input
+                  type="text"
+                  id="FirstName"
+                  value={step1Data}
+                  onChange={(e) => setStep1Data(e.target.value)}
+                  className="mt-1 w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
+                  placeholder="i.e, Mike"
+                  required
+                />
+              </div>
+            </div>
+          </>
         )}
         {activeStep === 2 && (
-          <TextField
-            label="Ad"
-            variant="outlined"
-            value={step3Data}
-            onChange={(e) => setStep3Data(e.target.value)}
-            className="w-full p-2 border rounded-md" // Apply Tailwind CSS classes here
-          />
+          <>
+            <div className=' grid grid-cols-2 px-10 gap-4 mt-10'>
+              <div>
+                <label
+                  htmlFor="FirstName"
+                  className="block text-sm font-medium text-white"
+                >
+                  Image Link1
+                </label>
+                <input
+                  type="text"
+                  id="FirstName"
+                  value={step1Data}
+                  onChange={(e) => setStep1Data(e.target.value)}
+                  className="mt-1 w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
+                  placeholder="i.e, Mike"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="FirstName"
+                  className="block text-sm font-medium text-white"
+                >
+                  Image Link2
+                </label>
+                <input
+                  type="text"
+                  id="FirstName"
+                  value={step1Data}
+                  onChange={(e) => setStep1Data(e.target.value)}
+                  className="mt-1 w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
+                  placeholder="i.e, Mike"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="FirstName"
+                  className="block text-sm font-medium text-white"
+                >
+                  Image Link3
+                </label>
+                <input
+                  type="text"
+                  id="FirstName"
+                  value={step1Data}
+                  onChange={(e) => setStep1Data(e.target.value)}
+                  className="mt-1 w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
+                  placeholder="i.e, Mike"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="FirstName"
+                  className="block text-sm font-medium text-white"
+                >
+                  Image Link4
+                </label>
+                <input
+                  type="text"
+                  id="FirstName"
+                  value={step1Data}
+                  onChange={(e) => setStep1Data(e.target.value)}
+                  className="mt-1 w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
+                  placeholder="i.e, Mike"
+                  required
+                />
+              </div>
+            </div>
+          </>
+        )}
+        {activeStep === 3 && (
+          <>
+            <div className=' grid grid-cols-2 px-10 gap-4 mt-10'>
+            <div class="flex items-center justify-center w-full">
+              <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                  </svg>
+                  <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                  <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                </div>
+                <input id="dropzone-file" type="file" class="hidden" />
+              </label>
+            </div>
+              <div>
+                <div>
+                <label
+                  htmlFor="FirstName"
+                  className="block text-sm font-medium text-white"
+                >
+                3D Model CDN 
+                </label>
+                <input
+                  type="text"
+                  id="FirstName"
+                  value={step1Data}
+                  onChange={(e) => setStep1Data(e.target.value)}
+                  className="mt-1 w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
+                  placeholder="i.e, Mike"
+                  required
+                />
+                </div>
+                <div>
+                <label
+                  htmlFor="FirstName"
+                  className="block text-sm font-medium text-white"
+                >
+                  FixedScale Value
+                </label>
+                <input
+                  type="text"
+                  id="FirstName"
+                  value={step1Data}
+                  onChange={(e) => setStep1Data(e.target.value)}
+                  className="mt-1 w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
+                  placeholder="i.e, Mike"
+                  required
+                />
+                </div>
+                <div>
+                <label
+                  htmlFor="FirstName"
+                  className="block text-sm font-medium text-white"
+                >
+                  Image Link1
+                </label>
+                <input
+                  type="text"
+                  id="FirstName"
+                  value={step1Data}
+                  onChange={(e) => setStep1Data(e.target.value)}
+                  className="mt-1 w-full rounded-md border  bg-white text-sm shadow-sm p-2 "
+                  placeholder="i.e, Mike"
+                  required
+                /> 
+                </div>
+              </div>
+            </div>
+          </>
         )}
         {allStepsCompleted() ? (
           <React.Fragment>
@@ -194,29 +431,29 @@ export default function AddCarPopup() {
           </React.Fragment>
         ) : (
           <React.Fragment>
-            <Typography sx={{ mt: 2, mb: 1, py: 1 }}>
+            {/* <Typography sx={{ mt: 2, mb: 1, py: 1 }}>
               Step {activeStep + 1}
-            </Typography>
+            </Typography> */}
             <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
               <Button
                 color="inherit"
                 disabled={activeStep === 0}
                 onClick={handleBack}
-                sx={{ mr: 1 }}
+                sx={{ mr: 1, color: 'white' }}
               >
                 Back
               </Button>
               <Box sx={{ flex: '1 1 auto' }} />
-              <Button onClick={handleNext} sx={{ mr: 1 }}>
+              <Button onClick={handleNext} sx={{ mr: 1, color: 'white' }}>
                 Next
               </Button>
               {activeStep !== steps.length &&
                 (completed[activeStep] ? (
-                  <Typography variant="caption" sx={{ display: 'inline-block' }}>
-                    Step {activeStep + 1} already completed
+                  <Typography variant="caption" sx={{ display: 'inline-block', color: 'white' }}>
+                    {/* Step {activeStep + 1} already completed */}
                   </Typography>
                 ) : (
-                  <Button onClick={handleComplete}>
+                  <Button sx={{ color: 'white' }} onClick={handleComplete}>
                     {completedSteps() === totalSteps() - 1
                       ? 'Finish'
                       : 'Complete Step'}
