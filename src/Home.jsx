4cb3e20e9b72596c6121925e9aad70d338e96b56
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2023-09-08 22:58:15 +0500

    See our Collection Button updated

--- src/Home.jsx
+++ src/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
       >
         {/* <img className=" absolute w-full -z-50" src={Background} alt="" srcset="" /> */}
         <div className=" max-[600px]:-translate-y-8 max-[440px]:-translate-y-0">
-        <Header/>
+          <Header />
         </div>
 
         <div
@@ -44,31 +44,33 @@ const Home = () => {
         >
           <div className="">
             <h1 className=" text-6xl w-[550px] max-[650px]:w-auto max-[1000px]:text-4xl max-[440px]:text-3xl  font-custom font-medium  text-white">
-            EveMotors Where We Care About Your Vehicle     </h1>
+              EveMotors Where We Care About Your Vehicle     </h1>
 
             <p className="mt-4 w-[350px] max-[440px]:w-auto max-[440px]:text-base  pr-8 text-xl text-white">
               Embark on Your Journey of Luxury and Performance: Discover Your Perfect Ride with Us.
             </p>
           </div>
           <div className=" absolute   max-[600px]:relative max-[600px]:flex max-[600px]:flex-col max-[600px]:gap-4 mt-8   text-center">
-              <a
-                href=".list"
-                className="inline-block shrink-0 rounded-[13px]    bg-[#FF001D] px-12 py-3 max-[600px]:px-6  mr-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-orange-red focus:outline-none focus:ring active:text-orange-red"
-              >
-                See our collection
-              </a>
+            <a
+              href="#list"
+              className="inline-block shrink-0 rounded-[13px]    bg-[#FF001D] px-12 py-3 max-[600px]:px-6  mr-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-orange-red focus:outline-none focus:ring active:text-orange-red"
+            >
+              See our collection
+            </a>
 
-              <Link to="/register" className="inline-block shrink-0 rounded-[13px] border border-white  px-12 py-3 mr-3 text-sm font-medium text-white hover:border-[#1E1E1E] transition hover:bg-[#1E1E1E] hover:text-orange-red focus:outline-none focus:ring active:text-orange-red"
-              >
-                Sign Up
-              </Link>
-            </div>
+            <Link to="/register" className="inline-block shrink-0 rounded-[13px] border border-white  px-12 py-3 mr-3 text-sm font-medium text-white hover:border-[#1E1E1E] transition hover:bg-[#1E1E1E] hover:text-orange-red focus:outline-none focus:ring active:text-orange-red"
+            >
+              Sign Up
+            </Link>
+          </div>
         </div>
       </section>
-      <div className="list">
-      <CarList />
+
+      <div id="list">
+        <CarList />
       </div>
-      <Footer/>
+
+      <Footer />
     </>
   );
 };
