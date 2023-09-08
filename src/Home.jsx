05ecdb30b54f962dea5995a1f7c40b95bfa88f47
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2023-09-09 02:27:50 +0500

    Home updated

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 import { FetchedCars } from "./services/FetchedCars.service.js";
 import { Link } from "react-router-dom";
 import Background from '../src/assets/mainbackground.png';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 
 const Home = () => {
@@ -77,34 +77,34 @@ const Home = () => {
 
       <div id="list">
         {getCars.map((car) => {
-           <div className=" ">
-           <div className=" mx-8 max-[440px]:mx-4 mt-6 grid grid-cols-3 max-[1200px]:grid-cols-2 max-[750px]:grid-cols-1 gap-4">
-               
-               <Link to="/car/supra-mk4" className="group h-[243px] max-[440px]:h-[280px] overflow-hidden  bg-[#1E1E1E] hover:bg-prime rounded-[24px] relative flex  justify-start items-center">
-                   <div className=" absolute right-4 z-50   flex transition-all">
-                       <img className=" group-hover:w-96 transition-all w-64 z-50" src="supra-mk4.png" alt="" srcset="" />
-                   </div>
-                   <div className=" flex h-full py-6 px-6 flex-col justify-between items-start">
-                       <h3 className="  text-4xl  font-custom font-bold uppercase text-white">{car.makeModel}</h3>
-                       <div className=" mt-8">
-                           <div className="">
-                               <div className=" ">
-                                   <p className=" text-white">
-                                       1997 - JAPAN
-                                   </p>
-                                   <p className=" text-white">
-                                       Registered in Karachi
-                                   </p>
-                               </div>
-                           </div>
-                       </div>
-                   </div>
-               </Link>
-               
-           </div>
-           
-       </div>
+          return (
+            <div className=" " key={car._id}>
+              <div className=" mx-8 max-[440px]:mx-4 mt-6 grid grid-cols-3 max-[1200px]:grid-cols-2 max-[750px]:grid-cols-1 gap-4">
+                <Link to={`/car/${car._id}`} className="group h-[243px] max-[440px]:h-[280px] overflow-hidden  bg-[#1E1E1E] hover:bg-prime rounded-[24px] relative flex  justify-start items-center">
+                  <div className=" absolute right-4 z-50   flex transition-all">
+                    <img className=" group-hover:w-96 transition-all w-64 z-50" src="supra-mk4.png" alt="" srcSet="" />
+                  </div>
+                  <div className=" flex h-full py-6 px-6 flex-col justify-between items-start">
+                    <h3 className="  text-4xl  font-custom font-bold uppercase text-white">{car.makeModel}</h3>
+                    <div className=" mt-8">
+                      <div className="">
+                        <div className=" ">
+                          <p className=" text-white">
+                            {car.intro}
+                          </p>
+                          <p className=" text-white">
+                            Registered in {car.registeredIn}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          );
         })}
+
       </div>
 
       <Footer />
