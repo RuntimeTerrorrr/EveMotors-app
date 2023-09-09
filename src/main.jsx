151b2da423fd93fa4c4766e2d34fa90f84dd204e
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2023-09-09 17:36:56 +0500

    Main updated

--- src/main.jsx
+++ src/main.jsx
@@ -6,22 +6,19 @@ import Register from './Register.jsx'
 import Home from './Home.jsx'
 import FormDashboard from './Dashboard.jsx';
 import './index.css'
-import AddACar from './AddACar.jsx';
 import CarDetail from './CarDetail.jsx';
-import Carlisting from './Carlisting.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Home />} />
-        <Route path="/car/:carId" component={CarDetail} />
-
+        <Route path="/car/:carId" component={<CarDetail />} />
         <Route path="/dashboard" element={<FormDashboard />} />
-        <Route path="/addacar" element={<AddACar />} />
-        <Route path="/carlisting" element={<Carlisting />} />
+
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
