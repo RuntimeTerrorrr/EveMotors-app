989b98b80765b9cbe28836ef723df96c0d076102
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2023-09-09 19:53:25 +0500

    LoaderAnimation Removed

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,8 +4,6 @@ import { Link } from "react-router-dom";
 import Background from '../src/assets/mainbackground.png';
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
-import LoaderAnimation from "./components/Loader.jsx";
-
 
 const Home = () => {
 
@@ -80,37 +78,37 @@ const Home = () => {
 
       <div id="list">
 
-          <div className=" " >
-            <div className=" mx-8 max-[440px]:mx-4 mt-6 grid grid-cols-3 max-[1200px]:grid-cols-2 max-[750px]:grid-cols-1 gap-4">
+        <div className=" " >
+          <div className=" mx-8 max-[440px]:mx-4 mt-6 grid grid-cols-3 max-[1200px]:grid-cols-2 max-[750px]:grid-cols-1 gap-4">
 
-              {getCars.map((car) => {
-                return (
-                  <Link key={car._id} to={`/car/${car._id}`} className="group h-[243px] max-[440px]:h-[280px] overflow-hidden  bg-[#1E1E1E] hover:bg-prime rounded-[24px] relative flex  justify-start items-center">
-                    <div className=" absolute right-4 z-50   flex transition-all">
-                      <img className=" group-hover:w-96 transition-all w-64 z-50" src="supra-mk4.png" alt="" srcSet="" />
-                    </div>
-                    <div className=" flex h-full py-6 px-6 flex-col justify-between items-start">
-                      <h3 className="  text-4xl  font-custom font-bold uppercase text-white">{car.makeModel}</h3>
-                      <div className=" mt-8">
-                        <div className="">
-                          <div className=" ">
-                            <p className=" text-white">
-                              {car.registeredIn}
-                            </p>
-                            <p className=" text-white">
-                              Registered in {car.assembledIn}
-                            </p>
-                          </div>
+            {getCars.map((car) => {
+              return (
+                <Link key={car._id} to={`/car/${car._id}`} className="group h-[243px] max-[440px]:h-[280px] overflow-hidden  bg-[#1E1E1E] hover:bg-prime rounded-[24px] relative flex  justify-start items-center">
+                  <div className=" absolute right-4 z-50   flex transition-all">
+                    <img className=" group-hover:w-96 transition-all w-64 z-50" src="supra-mk4.png" alt="" srcSet="" />
+                  </div>
+                  <div className=" flex h-full py-6 px-6 flex-col justify-between items-start">
+                    <h3 className="  text-4xl  font-custom font-bold uppercase text-white">{car.makeModel}</h3>
+                    <div className=" mt-8">
+                      <div className="">
+                        <div className=" ">
+                          <p className=" text-white">
+                            {car.registeredIn}
+                          </p>
+                          <p className=" text-white">
+                            Registered in {car.assembledIn}
+                          </p>
                         </div>
                       </div>
                     </div>
-                  </Link>
+                  </div>
+                </Link>
 
-                );
-              })}
-            </div>
-
+              );
+            })}
           </div>
+
+        </div>
       </div >
 
       <Footer />
